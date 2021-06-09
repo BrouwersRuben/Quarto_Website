934677a1a41f44d9c72a57c3599630e3f2431a07
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2021-06-09 18:29:31 +0200

    final exam preparation

--- js/Download.js
+++ js/Download.js
@@ -1,6 +1,6 @@
 onload = () => {
     var a = document.createElement("a");
-    a.setAttribute("href", "/download/Game/test.txt");
+    a.setAttribute("href", "/download/Game/Quarto.jar");
     a.setAttribute("rel", "noopener");
     a.setAttribute("target", "_blank")
     document.body.appendChild(a);
